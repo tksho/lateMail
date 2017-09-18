4464d29e6342837c4cd72c6912a295e44974668c
sho_takahashi <tksho112@gmail.com>
2017-09-18 17:03:04 +0900

    設定メニュー一覧表示

--- lateMail/LeftViewController.swift
+++ lateMail/LeftViewController.swift
@@ -8,28 +8,40 @@
 
 import UIKit
 
-class LeftViewController: UIViewController {
+class LeftViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet var tableView: UITableView!
+    var settings = ["トップ","宛先設定","件名設定","本文設定","本文自動作成"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
 
-        // Do any additional setup after loading the view.
+        // TableViewの不要行を消す
+        tableView.tableFooterView = UIView()
+
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        // Cellの高さを決める
+        return 60
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsCell")!
+        cell.textLabel?.text = settings[indexPath.row]
+        return cell
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settings.count
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
