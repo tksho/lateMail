df1f81319dd07b361c4b70caab453a4f4858c8e0
sho_takahashi <tksho112@gmail.com>
2017-09-23 10:57:46 +0900

    メニューから各設定画面へ遷移できるように

--- lateMail/LeftViewController.swift
+++ lateMail/LeftViewController.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class LeftViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
-    @IBOutlet var tableView: UITableView!
-    var settings = ["トップ","宛先設定","件名設定","本文設定","本文自動作成"]
+class LeftViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
     
+    @IBOutlet var tableView: UITableView!
+    var leftMenuList = ["プロフィール設定","メール宛先設定","件名編集","本文編集","自動作成設定"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,7 +21,34 @@ class LeftViewController: UIViewController, UITableViewDelegate, UITableViewData
 
         // TableViewの不要行を消す
         tableView.tableFooterView = UIView()
+    }
+    
+    // メニューがクリックされたら画面遷移
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 
+        if( leftMenuList[indexPath.row] == "プロフィール設定" ) {
+            self.performSegue(withIdentifier: "toProfileEdit", sender: nil)
+        }
+        else if( leftMenuList[indexPath.row] == "メール宛先設定" ) {
+            self.performSegue(withIdentifier: "toMailAddressEdit", sender: nil)
+        }
+        else if( leftMenuList[indexPath.row] == "件名編集" ) {
+            self.performSegue(withIdentifier: "toTitleEdit", sender: nil)
+        }
+        else if( leftMenuList[indexPath.row] == "本文編集" ) {
+            self.performSegue(withIdentifier: "toBodyEdit", sender: nil)
+        }
+        else if( leftMenuList[indexPath.row] == "自動作成設定" ) {
+            self.performSegue(withIdentifier: "toAutoSetting", sender: nil)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let indexPathForSelectedRow = tableView.indexPathForSelectedRow {
+            tableView.deselectRow(at: indexPathForSelectedRow, animated: true)
+        }
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -31,12 +58,12 @@ class LeftViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsCell")!
-        cell.textLabel?.text = settings[indexPath.row]
+        cell.textLabel?.text = leftMenuList[indexPath.row]
         return cell
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return settings.count
+        return leftMenuList.count
     }
     
     override func didReceiveMemoryWarning() {
